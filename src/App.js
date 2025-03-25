5949a89fc59323b2f2be7d1a274193594453101d
ravi-exonect <ravi.dholiya@exonect.com>
2025-03-25 18:40:40 +0530

    edit profile, order details, register, signuppopup  added .

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-  import logo from './logo.svg';
-  import './App.css';
-  import { BrowserRouter, Route, Routes } from 'react-router-dom';
-  import Home from './Components/Home';
-  import Products from './Components/Products';
-  import ProductDetails from './Components/ProductsDetails/index';
-  import Blog from './Components/Blog';
-  import BlogDetails from './Components/Blog Details';
-  import Contact from './Components/Contact Us';
-  import AboutUs from './Components/About Us';
-  import CartPopup from './Components/Add to Cart';
-  import CheckoutPage from './Components/Check Out';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import Products from './Components/Products';
+import ProductDetails from './Components/ProductsDetails/index';
+import Blog from './Components/Blog';
+import BlogDetails from './Components/Blog Details';
+import Contact from './Components/Contact Us';
+import AboutUs from './Components/About Us';
+import CartPopup from './Components/Add to Cart';
+import CheckoutPage from './Components/Check Out';
+import OrderDetails from './Components/Order';
+import EditProfile from './Components/EditProfile';
 
-  function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/product-details" element={<ProductDetails />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog-details" element={<BlogDetails />} />
-          <Route path="/contact-us" element={<Contact />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/add_to_cart" element={<CartPopup />} />
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/Editprofile" element={<EditProfile />} />
+        <Route path="/Order" element={<OrderDetails/>} />
+        <Route path="/product-details" element={<ProductDetails />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog-details" element={<BlogDetails />} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/add_to_cart" element={<CartPopup />} />
 
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-  export default App;
+export default App;
