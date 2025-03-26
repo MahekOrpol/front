525118ffd23fb004db5c493bf855a6b222777ad4
ravi-exonect <ravi.dholiya@exonect.com>
2025-03-26 09:19:38 +0530

    Revert "edit profile, order details, register, signuppopup  added ."
    This reverts commit 5949a89fc59323b2f2be7d1a274193594453101d.

--- src/Pages/Header/index.js
+++ src/Pages/Header/index.js
@@ -5,21 +5,11 @@ import logo from "../../Images/Group 1597884561.png";
 import usericon from "../../Images/Group.png";
 import { CiHeart, CiSearch } from "react-icons/ci";
 import { IoBagHandleOutline, IoClose, IoMenu } from "react-icons/io5";
-import { HiOutlineShoppingBag } from "react-icons/hi";
-import { Form, useNavigate } from "react-router-dom";
-import { BiSearch } from "react-icons/bi";
-import { FiHeart } from "react-icons/fi";
-import InputGroup from 'react-bootstrap/InputGroup';
-import { FormControl } from "react-bootstrap";
-import SignInPopup from "../../Components/SignupPopup/index";
-import RegisterPopup from "../../Components/RegisterPopup";
+import { useNavigate } from "react-router-dom";
 
 const Header = ({ openCart }) => {
   const navigate = useNavigate();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const [isProfilePopupOpen, setIsProfilePopupOpen] = useState(false);
-  const [isSignupPopupOpen, setIsSignupPopupOpen] = useState(false);
-  const [isRegisterPopupOpen, setIsRegisterPopupOpen] = useState(false);
 
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
@@ -36,38 +26,41 @@ const Header = ({ openCart }) => {
         <p className="header_text pt-2">Shop Gold and Diamond Jewellery</p>
       </div>
 
-      <div className="heder_sec_main d-flex align-items-center justify-content-lg-end sdcsd_ss_ddd container-fluid">
+      <div className="heder_sec_main d-flex align-items-center sdcsd_ss_ddd">
         {/* 🟢 Menu Icon for Mobile */}
         <div className="menu-icon d-xl-none" onClick={toggleDrawer}>
           <IoMenu size={30} />
         </div>
 
-        <InputGroup className="d-lg-flex input-group1" style={{ borderRadius: "10px", marginTop: "6px" }}>
-          <FormControl placeholder="Search Products Here" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-          <span className="p-3 rounded-r-full d-flex align-items-center justify-content-center search_hbdhj bg_prime search-button" style={{ height: "38px", borderRadius: "0px 10px 10px 0px" }}>
-            <BiSearch size={25} />
-          </span>
-        </InputGroup>
+        {/* Desktop Navigation */}
+        <div className="d-none d-lg-flex align-items-center sdchy_88__sxsxs" style={{ marginLeft: "7rem", borderRadius: "10px" }}>
+          <input
+            type="text"
+            placeholder="Search Product Here"
+            className="flex-1 px-4 py-2 outline-none text-gray-700" style={{ width: "100%", borderRadius: "10px 0px 0px 10px" }}
+          />
+          <button className="p-3 rounded-r-full d-flex align-items-center justify-content-center search_hbdhj bg_prime" style={{ height: "2.7rem", borderRadius: "0px 10px 10px 0px" }}>
+            <FaSearch />
+          </button>
+        </div>
 
         {/* Logo */}
-        <div className="d-flex justify-content-center w-lg-50 hedr_sss" style={{ cursor: "pointer" }}>
+        <div className="d-flex justify-content-center w-50 hedr_sss" style={{ cursor: "pointer" }}>
           <img src={logo} onClick={() => navigate('/')} alt="Logo" />
         </div>
 
-        <div className="w-25 d-none d-lg-flex align-items-center justify-content-end gap-5 dcxde_asx485">
-          <div className="user_icon d-flex align-items-center" style={{ gap: "9px" }} onClick={() => setIsSignupPopupOpen(true)}>
-            <img src={require("../../Images/profile1.png")} alt="User Icon" />
+        {/* Icons Section (Desktop Only) */}
+        <div className="w-25 d-none d-lg-flex align-items-center gap-5 dcxde_asx485">
+          <div className="user_icon gap-3 d-flex align-items-center">
+            <img src={usericon} alt="User Icon" />
             <div className="d-flex flex-column align-items-center pt-2">
               <span className="sign_txt w-100">Sign In</span>
               <span className="acco9_text w-100">Account</span>
             </div>
           </div>
-          {isSignupPopupOpen && (
-            <SignInPopup isOpen={isSignupPopupOpen} onClose={() => setIsSignupPopupOpen(false)} />
-          )}
-
-          <div><FiHeart size={27} /></div>
-          <div><HiOutlineShoppingBag size={30} onClick={openCart} /></div>
+          {/* <div><CiSearch size={25} /></div> */}
+          <div><CiHeart size={25} /></div>
+          <div><IoBagHandleOutline size={25} onClick={openCart}/></div>
         </div>
       </div>
       <div className="dsn_mdcm">
@@ -76,10 +69,10 @@ const Header = ({ openCart }) => {
           <div className="header_list_tcty mx-4 my-2" onClick={() => handleCategoryClick('Earrings')}>Earrings</div>
           <div className="header_list_tcty mx-4 my-2" onClick={() => handleCategoryClick('Pendant')}>Pendant</div>
           <div className="header_list_tcty mx-4 my-2" onClick={() => handleCategoryClick('Bracelet')}>Bracelet</div>
-          <div className="header_list_tcty mx-4 my-2" onClick={() => setIsProfilePopupOpen(!isProfilePopupOpen)}>Custom Jewellery</div>
+          <div className="header_list_tcty mx-4 my-2">Custom Jewellery</div>
         </div>
       </div>
-      {/* <div style={{ borderTop: "1px solid #797979",margin:"5px" }}></div> */}
+
       {/* 🟢 Mobile Drawer */}
       <div className={`mobile-drawer ${isDrawerOpen ? "open" : ""}`}>
 
@@ -96,16 +89,10 @@ const Header = ({ openCart }) => {
         </div>
 
         {/* 🟢 Icons Section Inside Drawer (Mobile Only) */}
-        <div className="user_icon gap-3 d-flex align-items-center ps-3" onClick={() => {
-          setIsDrawerOpen(false); // Close the drawer
-          setIsRegisterPopupOpen(true); // Open RegisterPopup
-        }}>
-          {isRegisterPopupOpen && (
-            <RegisterPopup isOpen={isRegisterPopupOpen} onClose={() => setIsRegisterPopupOpen(false)} />
-          )}
+        <div className="user_icon gap-3 d-flex align-items-center ps-3">
           <img src={usericon} alt="User Icon" />
           <div className="d-flex flex-column align-items-center pt-2">
-            <span className="sign_txt w-100" >Sign In</span>
+            <span className="sign_txt w-100">Sign In</span>
             <span className="acco9_text w-100">Account</span>
           </div>
         </div>
