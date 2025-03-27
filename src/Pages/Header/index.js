27dfbaf51eff496e2f795d1d4f519b5680ae0e01
ravi-exonect <ravi.dholiya@exonect.com>
2025-03-27 10:21:53 +0530

    space add

--- src/Pages/Header/index.js
+++ src/Pages/Header/index.js
@@ -105,6 +105,7 @@ const [isProfilePopupOpen, setIsProfilePopupOpen] = useState(false);
       <div className="dsn_mdcm">
         <div
           className="d-flex align-items-center justify-content-center jhdb_dhvh pt-1 pb-1 mt-2"
+          
           style={{ borderTop: "1px solid #797979" }}
         >
           <div
