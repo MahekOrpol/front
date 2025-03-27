90139704ee7f9498312e213c3acc17a8f7c0f4b4
ravi-exonect <ravi.dholiya@exonect.com>
2025-03-27 09:48:35 +0530

    chnage

--- src/Pages/Header/index.js
+++ src/Pages/Header/index.js
@@ -5,6 +5,7 @@ import logo from "../../Images/Group 1597884561.png";
 import usericon from "../../Images/Group.png";
 import { CiHeart, CiSearch } from "react-icons/ci";
 import { IoBagHandleOutline, IoClose, IoMenu } from "react-icons/io5";
+<<<<<<< Updated upstream
 import { useNavigate } from "react-router-dom";
 import { TfiMenuAlt } from "react-icons/tfi";
 import { FiSearch } from "react-icons/fi";
@@ -19,10 +20,24 @@ import { FormControl, InputGroup } from "react-bootstrap";
 
 import SignInPopup from "../../Components/SignupPopup/index";
 import RegisterPopup from "../../Components/RegisterPopup";
+=======
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { Form, useNavigate } from "react-router-dom";
+import { BiSearch } from "react-icons/bi";
+import { FiHeart } from "react-icons/fi";
+import InputGroup from 'react-bootstrap/InputGroup';
+import { FormControl } from "react-bootstrap";
+import SignInPopup from "../../Components/SignupPopup/index";
+import RegisterPopup from "../../Components/RegisterPopup/index";
+>>>>>>> Stashed changes
 
 const Header = ({ openCart }) => {
   const navigate = useNavigate();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+<<<<<<< Updated upstream
+=======
+  const [isProfilePopupOpen, setIsProfilePopupOpen] = useState(false);
+>>>>>>> Stashed changes
   const [isSignupPopupOpen, setIsSignupPopupOpen] = useState(false);
   const [isRegisterPopupOpen, setIsRegisterPopupOpen] = useState(false);
 
@@ -38,12 +53,19 @@ const Header = ({ openCart }) => {
       <div className="header_main">
         <p className="header_text pt-2">Shop Gold and Diamond Jewellery</p>
       </div>
+<<<<<<< Updated upstream
       <div className="heder_sec_main d-flex align-items-center sdcsd_ss_ddd">
         {/* :large_green_circle: Menu Icon for Mobile */}
+=======
+
+      <div className="heder_sec_main d-flex align-items-center justify-content-lg-end sdcsd_ss_ddd container-fluid">
+        {/* 🟢 Menu Icon for Mobile */}
+>>>>>>> Stashed changes
         <div className="menu-icon d-xl-none" onClick={toggleDrawer}>
           <LuTextSearch size={30} />
         </div>
 
+<<<<<<< Updated upstream
 
         {/* <InputGroup className="d-lg-flex input-group1" style={{ borderRadius: "10px", marginTop: "6px" }}>
           <FormControl placeholder="Search Products Here" aria-label="Recipient's username" aria-describedby="basic-addon2" />
@@ -52,10 +74,21 @@ const Header = ({ openCart }) => {
 
           </span>
         </InputGroup>
+=======
+        <InputGroup className="d-lg-flex input-group1" style={{ borderRadius: "10px", marginTop: "6px" }}>
+          <FormControl placeholder="Search Products Here" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+          <span className="p-3 rounded-r-full d-flex align-items-center justify-content-center search_hbdhj bg_prime search-button" style={{ height: "38px", borderRadius: "0px 10px 10px 0px" }}>
+            <BiSearch size={25} />
+          </span>
+        </InputGroup>
+
+        {/* Logo */}
+>>>>>>> Stashed changes
         <div className="d-flex justify-content-center w-lg-50 hedr_sss" style={{ cursor: "pointer" }}>
           <img src={logo} onClick={() => navigate('/')} alt="Logo" />
         </div> */}
 
+<<<<<<< Updated upstream
         <div className="w-25 justify-content-center d-flex ps-lg-5 xdcxscd_sercv">
           <div
             className="d-flex align-items-center sdchy_88__sxsxs"
@@ -87,6 +120,12 @@ const Header = ({ openCart }) => {
           <div className="user_icon gap-3 d-flex align-items-center d-none d-lg-block d-lg-flex" onClick={() => setIsSignupPopupOpen(true)} style={{cursor:'pointer'}}>
             <img src={usericon} alt="User Icon" />
             <div className="d-flex flex-column align-items-center pt-2" style={{ lineHeight: '21px' }}>
+=======
+        <div className="w-25 d-none d-lg-flex align-items-center justify-content-end gap-5 dcxde_asx485">
+          <div className="user_icon d-flex align-items-center" style={{ gap: "9px" }} onClick={() => setIsSignupPopupOpen(true)}>
+            <img src={require("../../Images/profile1.png")} alt="User Icon" />
+            <div className="d-flex flex-column align-items-center pt-2">
+>>>>>>> Stashed changes
               <span className="sign_txt w-100">Sign In</span>
               <span className="acco9_text w-100">Account</span>
             </div>
@@ -94,6 +133,7 @@ const Header = ({ openCart }) => {
           {isSignupPopupOpen && (
             <SignInPopup isOpen={isSignupPopupOpen} onClose={() => setIsSignupPopupOpen(false)} />
           )}
+<<<<<<< Updated upstream
           {/* <div><CiSearch size={25} /></div> */}
           <div className="d-block d-lg-none dsfcecv_dfvf">
             <FiSearch size={25} />
@@ -140,6 +180,24 @@ const Header = ({ openCart }) => {
         </div>
       </div>
       {/* :large_green_circle: Mobile Drawer */}
+=======
+
+          <div><FiHeart size={27} /></div>
+          <div><HiOutlineShoppingBag size={30} onClick={openCart} /></div>
+        </div>
+      </div>
+      <div className="dsn_mdcm">
+        <div className="d-flex align-items-center justify-content-center jhdb_dhvh pt-2 mt-2" style={{ borderTop: "1px solid #797979" }}>
+          <div className="header_list_tcty mx-4 my-2" onClick={() => handleCategoryClick('Rings')}>Rings</div>
+          <div className="header_list_tcty mx-4 my-2" onClick={() => handleCategoryClick('Earrings')}>Earrings</div>
+          <div className="header_list_tcty mx-4 my-2" onClick={() => handleCategoryClick('Pendant')}>Pendant</div>
+          <div className="header_list_tcty mx-4 my-2" onClick={() => handleCategoryClick('Bracelet')}>Bracelet</div>
+          <div className="header_list_tcty mx-4 my-2" onClick={() => setIsProfilePopupOpen(!isProfilePopupOpen)}>Custom Jewellery</div>
+        </div>
+      </div>
+      {/* <div style={{ borderTop: "1px solid #797979",margin:"5px" }}></div> */}
+      {/* 🟢 Mobile Drawer */}
+>>>>>>> Stashed changes
       <div className={`mobile-drawer ${isDrawerOpen ? "open" : ""}`}>
         <div className="drawer-header">
           <IoClose size={30} onClick={toggleDrawer} />
