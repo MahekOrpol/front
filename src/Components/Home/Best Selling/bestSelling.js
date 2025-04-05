023aad13d0151bfc65bacc97f9c02ca0f806876b
MahekOrpol <mahekorpolinfotech24@gmail.com>
2025-04-05 16:58:52 +0530

    chnge

--- src/Components/Home/Best Selling/bestSelling.js
+++ src/Components/Home/Best Selling/bestSelling.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import best from '../../../Images/best.png'
+import React from "react";
+import best from "../../../Images/best.png";
 const BestSelling = () => {
   return (
-    <div className='container'>
-      <div className='d-flex gap-2 align-items-center'>
-<div className='col-md-6'>
-    <img src={best}/>
-</div>
-<div className='col-md-6'></div>
+    <div className="container">
+      <div className="d-flex gap-2 align-items-center">
+        <div className="col-md-6">
+          <img src={best} />
+        </div>
+        <div className="col-md-6"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BestSelling
+export default BestSelling;
