5c4f7c2bd9abb5f85a7e68dca7f7285c65dae015
ravidholiya <dholiyaravi001@gmail.com>
2025-04-16 12:30:03 +0530

    - fix bug of Home banner video not load in safari

--- src/Components/Home/Dimond Jewellery/dimond.js
+++ src/Components/Home/Dimond Jewellery/dimond.js
@@ -3,7 +3,7 @@ import diamond from "../../../Videos/pinterestdownloader.mp4";
 import './dimond.css'
 import { useNavigate } from "react-router-dom";
 const DimondJewellery = () => {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const handleCategoryClick = (category) => {
     navigate(`/products?categoryName=${category}`);
   };
@@ -12,24 +12,36 @@ const DimondJewellery = () => {
       <div className="row g-2 align-items-stretch sdcsz_rokdscs">
         <div className="col-md-4 d-flex flex-column gap-2 dimond-section ">
           <div className="h-100">
-            <img src={require("../../../Images/rose.png")} className="w-100 h-100 diamond_img" onClick={() => handleCategoryClick("Rings")}/>
+            <img src={require("../../../Images/rose.png")} alt={"productImg"} className="w-100 h-100 diamond_img" onClick={() => handleCategoryClick("Rings")} />
           </div>
           <div className="h-100">
-            <img src={require("../../../Images/pen.png")} className="w-100 h-100 diamond_img" onClick={() => handleCategoryClick("Pendant")}/>
+            <img src={require("../../../Images/pen.png")} alt={"productImg"} className="w-100 h-100 diamond_img" onClick={() => handleCategoryClick("Pendant")} />
           </div>
         </div>
         <div className="col-md-4 d-flex flex-column dis_edjxfd">
           <div>
             {" "}
-            <video src={diamond} onClick={()=>navigate('/products')} className="w-100 h-100 object-fit-cover bg-white demoiin-video_sec" autoPlay loop muted />
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="w-100 h-100 object-fit-cover bg-white demoiin-video_sec"
+              onClick={() => navigate('/products')}
+            >
+              <source src={require("../../../Videos/pinterestdownloader.mp4")} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+
+            {/* <video src={require("../../../Videos/pinterestdownloader.mp4")} onClick={() => navigate('/products')} className="w-100 h-100 object-fit-cover bg-white demoiin-video_sec" autoPlay loop muted playsInline /> */}
           </div>
         </div>
         <div className="col-md-4 d-flex flex-column gap-2 dimond-section ">
           <div className="h-100">
-            <img src={require("../../../Images/bracletesss.png")} className="w-100 h-100 diamond_img" onClick={() => handleCategoryClick("Bracelet")}/>
+            <img src={require("../../../Images/bracletesss.png")} alt={"productImg"} className="w-100 h-100 diamond_img" onClick={() => handleCategoryClick("Bracelet")} />
           </div>
           <div className="h-100">
-            <img src={require("../../../Images/schbgdsccc.png")} className="w-100 h-100 diamond_img" onClick={() => handleCategoryClick("Earrings")}/>
+            <img src={require("../../../Images/schbgdsccc.png")} alt={"productImg"} className="w-100 h-100 diamond_img" onClick={() => handleCategoryClick("Earrings")} />
           </div>
         </div>
       </div>
