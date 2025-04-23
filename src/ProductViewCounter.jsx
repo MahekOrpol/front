b8d9cd8e1e18a0eb62357e2a080eb72f61ca34a1
MahekOrpol <mahekorpolinfotech24@gmail.com>
2025-04-23 15:40:45 +0530

    comment

--- src/ProductViewCounter.jsx
+++ src/ProductViewCounter.jsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { io } from 'socket.io-client';
+// import React, { useEffect, useState } from 'react';
+// import { io } from 'socket.io-client';
 
-// const socket = io("http://147.93.104.196:3000");
-const socket = io("http://192.168.1.4:3000");
+// // const socket = io("http://147.93.104.196:3000");
+// const socket = io("http://192.168.1.4:3000");
 
-const ProductViewCounter = ({ productId }) => {
-  const [viewerCount, setViewerCount] = useState(0);
+// const ProductViewCounter = ({ productId }) => {
+//   const [viewerCount, setViewerCount] = useState(0);
 
-  useEffect(() => {
-    // Join product room
-    socket.emit('join_product', productId);
+//   useEffect(() => {
+//     // Join product room
+//     socket.emit('join_product', productId);
 
-    socket.on('viewer_count', (count) => {
-      setViewerCount(count);
-    });
+//     socket.on('viewer_count', (count) => {
+//       setViewerCount(count);
+//     });
 
-    return () => {
-      socket.disconnect(); // Cleanup on unmount
-    };
-  }, [productId]);
+//     return () => {
+//       socket.disconnect(); // Cleanup on unmount
+//     };
+//   }, [productId]);
 
-  return (
-    <div style={{ fontWeight: 'bold' }}>
-      {viewerCount} customer{viewerCount !== 1 ? 's' : ''} viewing this product
-    </div>
-  );
-};
+//   return (
+//     <div style={{ fontWeight: 'bold' }}>
+//       {viewerCount} customer{viewerCount !== 1 ? 's' : ''} viewing this product
+//     </div>
+//   );
+// };
 
-export default ProductViewCounter;
+// export default ProductViewCounter;
