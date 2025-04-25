01894b36aa8889184e184906e30f5861ddaef7f1
MahekOrpol <mahekorpolinfotech24@gmail.com>
2025-04-25 19:03:01 +0530

    chnage

--- craco.config.js
+++ craco.config.js
@@ -1,18 +1,30 @@
-import TerserPlugin from 'terser-webpack-plugin';
-import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
-export const webpack = {
-  configure: (webpackConfig) => {
-    webpackConfig.optimization.minimize = true;
-    webpackConfig.optimization.minimizer = [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-          },
-        },
-      }),
-      new CssMinimizerPlugin(),
-    ];
-    return webpackConfig;
+import TerserPlugin from "terser-webpack-plugin";
+export default {
+  webpack: {
+    configure: (webpackConfig, { env }) => {
+      // Enable code splitting
+      webpackConfig.optimization.splitChunks = {
+        chunks: "all",
+        automaticNameDelimiter: "-",
+      };
+      if (env === "production") {
+        // Enable JavaScript minification with console.log removal
+        webpackConfig.optimization.minimize = true;
+        webpackConfig.optimization.minimizer = [
+          new TerserPlugin({
+            terserOptions: {
+              compress: {
+                drop_console: true, // Remove console.* in production
+              },
+              output: {
+                comments: false, // Remove comments
+              },
+            },
+            extractComments: false,
+          }),
+        ];
+      }
+      return webpackConfig;
+    },
   },
 };
