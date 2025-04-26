5f5df7afa70ccbac4db903420a8ed846e3676015
MahekOrpol <mahekorpolinfotech24@gmail.com>
2025-04-26 09:46:04 +0530

    add file

--- craco.config.js
+++ craco.config.js
@@ -1,24 +1,30 @@
 import TerserPlugin from "terser-webpack-plugin";
+
 export default {
+  style: {
+    postcss: {
+      plugins: [
+        require('autoprefixer'),
+        ...(process.env.NODE_ENV === 'production' ? [require('@fullhuman/postcss-purgecss')({
+          content: ['./src/**/*.js', './src/**/*.jsx', './public/index.html'],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        })] : []),
+      ],
+    },
+  },
   webpack: {
     configure: (webpackConfig, { env }) => {
-      // Enable code splitting
       webpackConfig.optimization.splitChunks = {
         chunks: "all",
         automaticNameDelimiter: "-",
       };
       if (env === "production") {
-        // Enable JavaScript minification with console.log removal
         webpackConfig.optimization.minimize = true;
         webpackConfig.optimization.minimizer = [
           new TerserPlugin({
             terserOptions: {
-              compress: {
-                drop_console: true, // Remove console.* in production
-              },
-              output: {
-                comments: false, // Remove comments
-              },
+              compress: { drop_console: true },
+              output: { comments: false },
             },
             extractComments: false,
           }),
