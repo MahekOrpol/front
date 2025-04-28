36589053e78278d62956e083740f1d687ed9e114
MahekOrpol <mahekorpolinfotech24@gmail.com>
2025-04-28 18:41:00 +0530

    whatsapp icon remove

--- src/Components/WhatsAppButton.js
+++ src/Components/WhatsAppButton.js
@@ -1,39 +1,39 @@
-// components/WhatsAppButton.jsx
-import React from "react";
-import { useLocation } from "react-router-dom";
+// // components/WhatsAppButton.jsx
+// import React from "react";
+// import { useLocation } from "react-router-dom";
 
-const WhatsAppButton = () => {
-  const location = useLocation();
-  const phoneNumber = "919081139039";
-  // const phoneNumber = "919099975424";
-  const currentUrl =
-    typeof window !== "undefined"
-      ? `${window.location.origin}${location.pathname}${location.search}`
-      : `https://yourdomain.com${location.pathname}${location.search}`;
+// const WhatsAppButton = () => {
+//   const location = useLocation();
+//   const phoneNumber = "919081139039";
+//   // const phoneNumber = "919099975424";
+//   const currentUrl =
+//     typeof window !== "undefined"
+//       ? `${window.location.origin}${location.pathname}${location.search}`
+//       : `https://yourdomain.com${location.pathname}${location.search}`;
 
-  const message = `${currentUrl} \n Hey! I need help with something.`;
-  const encodedMessage = encodeURIComponent(message);
+//   const message = `${currentUrl} \n Hey! I need help with something.`;
+//   const encodedMessage = encodeURIComponent(message);
 
-  return (
-    <a
-      href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`}
-      className="whatsapp-float"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img
-        src='/Images/whatsapp_3670051.png'
-        alt="WhatsApp"
-        style={{
-          width: "55px",
-          height: "55px",
-          borderRadius: "50px",
-          padding: "7px",
-        }}
-        className="whatsappIcon-mobile"
-      />
-    </a>
-  );
-};
+//   return (
+//     <a
+//       href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`}
+//       className="whatsapp-float"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       <img
+//         src='/Images/whatsapp_3670051.png'
+//         alt="WhatsApp"
+//         style={{
+//           width: "55px",
+//           height: "55px",
+//           borderRadius: "50px",
+//           padding: "7px",
+//         }}
+//         className="whatsappIcon-mobile"
+//       />
+//     </a>
+//   );
+// };
 
-export default WhatsAppButton;
+// export default WhatsAppButton;
