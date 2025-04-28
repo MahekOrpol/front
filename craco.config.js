62288cd73900575b8a4ca4fc663ff007fe335fdb
MahekOrpol <mahekorpolinfotech24@gmail.com>
2025-04-28 08:50:56 +0530

    set
    isProd = process.env.NODE_ENV === 'production'; // ✅ Correct way

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,7 @@ const CompressionPlugin = require('compression-webpack-plugin');
 const Critters = require('critters-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-const isProd =  'production';
+const isProd = process.env.NODE_ENV === 'production'; // ✅ Correct way
 
 module.exports = {
   webpack: {
