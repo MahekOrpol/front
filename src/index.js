b7611e50dfcb25935cae6a682f58b36632391ce7
MahekOrpol <mahekorpolinfotech24@gmail.com>
2025-04-30 12:05:18 +0530

    delete this file

--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './critical.css';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Defer non-critical CSS
-const loadNonCriticalCSS = () => {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = '/index.css';
-  document.head.appendChild(link);
-};
-
-// Create root and render app
-const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <React.StrictMode>  
+     <Provider store={store}> 
       <App />
-    </Provider>
+      </Provider>
   </React.StrictMode>
 );
 
-// Load non-critical resources after main content
-window.addEventListener('load', () => {
-  loadNonCriticalCSS();
-  
-  // Register service worker
-  if ('serviceWorker' in navigator) {
+// Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
         console.log('ServiceWorker registration successful');
@@ -36,14 +25,10 @@ window.addEventListener('load', () => {
       .catch(err => {
         console.log('ServiceWorker registration failed: ', err);
       });
-  }
+  });
+}
 
-  // Initialize performance monitoring
-  const reportWebVitals = async () => {
-    const { getCLS, getFID, getLCP } = await import('web-vitals');
-    getCLS(console.log);
-    getFID(console.log);
-    getLCP(console.log);
-  };
-  reportWebVitals();
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
