31015e269ca0265272dc4504042a1064dfa837ba
MahekOrpol <mahekorpolinfotech24@gmail.com>
2025-04-30 11:00:52 +0530

    chnage

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import './critical.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// Defer non-critical CSS
+const loadNonCriticalCSS = () => {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = '/index.css';
+  document.head.appendChild(link);
+};
+
+// Create root and render app
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>  
-     <Provider store={store}> 
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-      </Provider>
+    </Provider>
   </React.StrictMode>
 );
 
-// Register service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
+// Load non-critical resources after main content
+window.addEventListener('load', () => {
+  loadNonCriticalCSS();
+  
+  // Register service worker
+  if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
         console.log('ServiceWorker registration successful');
@@ -25,10 +36,14 @@ if ('serviceWorker' in navigator) {
       .catch(err => {
         console.log('ServiceWorker registration failed: ', err);
       });
-  });
-}
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // Initialize performance monitoring
+  const reportWebVitals = async () => {
+    const { getCLS, getFID, getLCP } = await import('web-vitals');
+    getCLS(console.log);
+    getFID(console.log);
+    getLCP(console.log);
+  };
+  reportWebVitals();
+});
